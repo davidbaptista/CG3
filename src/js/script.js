a182bfb8fd7d0942a5bb0d8a72f2b636963ab7bc
daviderino <david.baptista00@hotmail.com>
2020-10-29 13:41:20 +0000

    codigo base

---
+++ src/js/script.js
@@ -0,0 +1,90 @@
+/*global THREE, requestAnimationFrame, console*/
+
+var camera, scene, renderer;
+
+var geometry, material, mesh;
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+
+    scene.add(new THREE.AxisHelper(10));
+
+    createTable(0, 8, 0);
+    createBall(0, 0, 15);
+}
+
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+}
+
+function onResize() {
+    'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
+}
+
+function onKeyDown(e) {
+    'use strict';
+
+    switch (e.keyCode) {
+    case 65: //A
+    case 97: //a
+        scene.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                node.material.wireframe = !node.material.wireframe;
+            }
+        });
+        break;
+    case 69:  //E
+    case 101: //e
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxisHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+    }
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+}
+
+function animate() {
+    'use strict';
+
+    render();
+
+    requestAnimationFrame(animate);
+}
